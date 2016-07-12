28e66b3ad8d336fc8a7cc12c9ed5d92ab7281d3b
Harigarakumar <hariv3114>
2016-07-11 23:10:12 -0500

    Basic Template

---
+++ Login_with_Maven/src/main/java/com/prac/model/User.java
@@ -0,0 +1,13 @@
+
+package com.prac.model;
+
+/**
+ * Created by Harigarakumar_Velayudam
+ * on Jul 11, 2016
+ * @version 1.0
+ * @since Jul 11, 2016
+ */
+
+public class User {
+
+}
---
+++ Login_with_Maven/src/main/java/com/prac/view/LoginView.java
@@ -0,0 +1,13 @@
+
+package com.prac.view;
+
+/**
+ * Created by Harigarakumar_Velayudam
+ * on Jul 11, 2016
+ * @version 1.0
+ * @since Jul 11, 2016
+ */
+
+public class LoginView {
+
+}
